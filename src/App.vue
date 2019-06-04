60a761ce6add4fb815b5b276a564b797e0052730
Parkjunwoo <sinyoorang@gmail.com>
2019-06-04 22:59:15 +0900

    webpack-simple설치&Vuex적용

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div id="app">
+      Parent counter : {{ this.$store.state.counter }} <br>
+      <button @click="addCounter">+</button>
+      <button @click="subCounter">-</button>
+
+      <child></child>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Child from './Child.vue'
+
+  export default {
+    methods: {
+      addCounter() {
+        this.$store.state.counter++;
+      },
+      subCounter() {
+        this.$store.state.counter--;
+      }
+    },
+    components: {
+      'child': Child
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Child.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <hr>
+    Child counter : {{ this.$store.state.counter }} <br>
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
